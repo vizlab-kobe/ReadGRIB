fd9bf146beed22839009308396b318a0249586ea
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2016-07-26 16:28:19 +0900

    Modified.

--- App/Test/BinaryDataSection.cpp
+++ App/Test/BinaryDataSection.cpp
@@ -3,6 +3,38 @@
 #include <kvs/Type>
 
 
+namespace
+{
+
+float IBM2Float( const kvs::UInt8* n )
+{
+    long int mant = ( n[1] << 16 ) + ( n[2] << 8 ) + n[3];
+    if ( mant == 0 ) { return 0.0; }
+
+    int positive = ( n[0] & 0x80 ) == 0;
+    int power = (int)( n[0] & 0x7f ) - 64;
+    unsigned int abspower = power > 0 ? power : -power;
+
+    double exp = 16.0;
+    double value = 1.0;
+    while ( abspower )
+    {
+        if ( abspower & 1 ) { value *= exp; }
+        exp = exp * exp;
+        abspower >>= 1;
+    }
+
+    if ( power < 0 ) { value = 1.0 / value; }
+    value = value * mant / 16777216.0;
+    if ( positive == 0 ) { value = -value; }
+
+//    return float(value);
+    return value;
+}
+
+} // end of namespace
+
+
 namespace ReadGRIB
 {
 
@@ -16,6 +48,9 @@ BinaryDataSection::BinaryDataSection()
 void BinaryDataSection::print( std::ostream& os, const kvs::Indent& indent ) const
 {
     os << indent << "Length : " << m_length << std::endl;
+    os << indent << "Binary Scale factor E : " << m_scale_factor << std::endl;
+    os << indent << "Reference value R : " << m_ref_value << std::endl;
+    os << indent << "Number of bits : " << m_nbits << std::endl;
 }
 
 bool BinaryDataSection::read( std::ifstream& ifs )
@@ -29,6 +64,15 @@ bool BinaryDataSection::read( std::ifstream& ifs )
     }
 
     m_length = pack( buffer[0], buffer[1], buffer[2] );
+    m_scale_factor = pack( buffer[4], buffer[5] );
+    m_ref_value = ::IBM2Float( buffer.data() + 6 );
+    m_nbits = pack( buffer[10] );
+
+    const bool is_float = ( ( buffer[3] & 32 ) == 0 );
+    const bool has_more_flags = ( ( buffer[3] & 16 ) == 0 );
+    const int unused_bits = int( buffer[3] & 15 );
+    const int starting_point = 11 + has_more_flags * 3;
+    const int nvalues = ( m_nbits == 0 ) ? 0 : ( ( m_length - starting_point ) * 8 - unused_bits ) / m_nbits;
 
     return true;
 }
--- App/Test/BinaryDataSection.h
+++ App/Test/BinaryDataSection.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <kvs/Indent>
+#include <kvs/AnyValueArray>
 #include "Section.h"
 
 
@@ -15,11 +16,21 @@ class BinaryDataSection : public Section
 {
 private:
 
-    int m_length; ///< Length of the binary data section
+    int m_length; ///< Length of the binary data section (octet 1-3)
+    int m_scale_factor; ///< binary scale factor E (octet 5-6)
+    float m_ref_value; ///< reference value R (octet 7-10)
+    int m_nbits; ///< number of bits into which datum point is packed (octet 11)
+    kvs::AnyValueArray m_data;
 
 public:
 
     BinaryDataSection();
+
+    int length() const { return m_length; }
+    int scaleFactor() const { return m_scale_factor; }
+    float refValue() const { return m_ref_value; }
+    int numberOfBits() const { return m_nbits; }
+    const kvs::AnyValueArray& data() const { return m_data; }
 
     void print( std::ostream& os, const kvs::Indent& indent = kvs::Indent(0) ) const;
     bool read( std::ifstream& ifs );
